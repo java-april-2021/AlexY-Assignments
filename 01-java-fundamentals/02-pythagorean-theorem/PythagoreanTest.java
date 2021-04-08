01f4beae4e97d880b65ee0f0698f5d4c608220f0
Alex Yom <alexyom24@gmail.com>
2021-04-07 22:41:04 -0400

    pythagorean test

--- 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -1,6 +1,8 @@
 public class PythagoreanTest {
     public static void main(String[] args) {
-        Pythagorean c = new Pythagorean();
-        
+        int a = 3;
+        int b = 5;
+        Pythagorean pyth = new Pythagorean();
+        System.out.println(pyth.calculateHypotenuse(a, b));
     }
 }
