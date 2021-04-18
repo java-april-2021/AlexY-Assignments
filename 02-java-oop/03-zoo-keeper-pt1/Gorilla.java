40c3288ee225cf9db3945966db7b6346e9bf56f7
Alex Yom <alexyom24@gmail.com>
2021-04-17 23:03:14 -0400

    zoo1

---
+++ 02-java-oop/03-zoo-keeper-pt1/Gorilla.java
@@ -0,0 +1,17 @@
+public class Gorilla extends Mammal {
+    public Gorilla() {
+        super(100);
+    }
+    public void throwSomething() {
+        System.out.println("Gorilla threw something; -5 Energy");
+        energyLevel -= 5;
+    }
+    public void eatBanana() {
+        System.out.println("Gorilla eats banana and is happy; +10 Energy");
+        energyLevel += 10;
+    }
+    public void climb() {
+        System.out.println("Climbing the tree; -10 Energy");
+        energyLevel -= 10;
+    }
+}
---
+++ 02-java-oop/03-zoo-keeper-pt1/GorillaTest.java
@@ -0,0 +1,15 @@
+public class GorillaTest {
+    public static void main(String[] args) {
+        Gorilla Harambe = new Gorilla();
+        Harambe.displayEnergy();
+        Harambe.throwSomething();
+        Harambe.throwSomething();
+        Harambe.throwSomething();
+        Harambe.displayEnergy();
+        Harambe.eatBanana();
+        Harambe.eatBanana();
+        Harambe.displayEnergy();
+        Harambe.climb();
+        Harambe.displayEnergy();
+    }
+}
