0f9e9d84095d3b7b2da61fdc7ac3ae7dd84fe5eb
Alex Yom <alexyom24@gmail.com>
2021-04-18 17:03:56 -0400

    bankaccount

--- 02-java-oop/07-bank-account/BankAccount.java
+++ 02-java-oop/07-bank-account/BankAccount.java
@@ -1,3 +1,65 @@
+import java.util.Random;
 public class BankAccount{
-    private String 
+    public static int numAccounts = 0;
+    public static double totalHoldings = 0;
+
+    private double checkingBalance;
+    private double savingsBalance;
+    private String accountNumber;
+
+    private static String generateId() {
+        String accountId = "";
+        Random r = new Random();
+        for(int i = 0; i < 10; i++){
+            accountId += r.nextInt(10);
+        }
+        return accountId;
+    }
+
+    public BankAccount() {
+        BankAccount.numAccounts++;
+        this.checkingBalance = 0;
+        this.savingsBalance = 0;
+        this.accountNumber = BankAccount.generateId();
+        System.out.println("Bank Account Number: " + accountNumber);
+    }
+
+    public double getCheckingBalance() {
+        return this.checkingBalance;
+    }
+
+    public double getSavingsBalance() {
+        return this.savingsBalance;
+    }
+
+    public void depositMoney(double amount, String account) {
+        if(account.equals("checking"))
+            this.checkingBalance += amount;
+        else if(account.equals("savings"))
+            this.savingsBalance += amount;
+        BankAccount.totalHoldings += amount;
+    }
+
+    public void withdrawMoney(double amount, String account) {
+        boolean success = false;
+        if(account.equals("checking")){
+            if(this.checkingBalance - amount >= 0){
+                success = true;
+                this.checkingBalance -= amount;
+            }
+        }
+        else if(account.equals("savings"))
+            if(this.savingsBalance - amount >= 0){
+                success = true;
+                this.savingsBalance -= amount;
+            }
+        if(success) {
+            BankAccount.totalHoldings -= amount;
+        }
+    }
+
+    public void displayAccountBalance() {
+        System.out.println(String.format("Checking: %.2f, Saving: %.2f", this.checkingBalance, this.savingsBalance));
+    }
+
 }
---
+++ 02-java-oop/07-bank-account/BankAccountTest.java
@@ -0,0 +1,9 @@
+public class BankAccountTest {
+    public static void main(String[] args) {
+        BankAccount me = new BankAccount();
+        me.depositMoney(100, "checking");
+        me.displayAccountBalance();
+        System.out.println(BankAccount.totalHoldings);
+        
+    }
+}
